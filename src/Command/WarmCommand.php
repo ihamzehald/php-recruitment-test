af6a2a00a8b99baf1a8c2ae4577cd8265b964096
Hamzeh al darawsheh <ihamzehald@gmail.com>
2018-09-25 09:25:53 +0300

    Modify warming process to use varnish server ip to visit the hosts

--- src/Command/WarmCommand.php
+++ src/Command/WarmCommand.php
@@ -5,6 +5,7 @@ namespace Snowdog\DevTest\Command;
 use Snowdog\DevTest\Model\PageManager;
 use Snowdog\DevTest\Model\WebsiteManager;
 use Symfony\Component\Console\Output\OutputInterface;
+use Snowdog\DevTest\Model\VarnishManager;
 
 class WarmCommand
 {
@@ -16,11 +17,18 @@ class WarmCommand
      * @var PageManager
      */
     private $pageManager;
+    /**
+     * @var PageManager
+     */
+    private $varnishManager;
 
-    public function __construct(WebsiteManager $websiteManager, PageManager $pageManager)
+    public function __construct(WebsiteManager $websiteManager,
+                                PageManager $pageManager,
+                                VarnishManager $varnishManager)
     {
         $this->websiteManager = $websiteManager;
         $this->pageManager = $pageManager;
+        $this->varnishManager = $varnishManager;
     }
 
     public function __invoke($id, OutputInterface $output)
@@ -31,11 +39,32 @@ class WarmCommand
 
             $resolver = new \Old_Legacy_CacheWarmer_Resolver_Method();
             $actor = new \Old_Legacy_CacheWarmer_Actor();
-            $actor->setActor(function ($hostname, $ip, $url) use ($output) {
+            $actor->setActor(function ($hostname, $ip, $url) use ($output, $website) {
+                $varnishServer = $this->varnishManager->getByWebsite($website);
+
+                /**
+                 * Note:
+                 * here if the website assigend to a varnish server it will use
+                 * the varnish IP to visit the website else it will work as it was
+                 * before and use the host IP, this was not in the requirements
+                 * but i find it logical and better than leaving the website without
+                 * warming.
+                 */
+                $wram_server_text = "";
+                if(isset($varnishServer['ip'])){
+                    $wram_server_text = "--> Using Varnish server ip to visit the host ({$varnishServer['ip']}).";
+                    $ip = $varnishServer['ip'];
+                }else{
+                    $wram_server_text = "--> Using the host IP to visit the host ({$ip}).";
+                }
+
+                $output->writeln($wram_server_text);
                 $output->writeln('Visited <info>http://' . $hostname . '/' . $url . '</info> via IP: <comment>' . $ip . '</comment>');
             });
             $warmer = new \Old_Legacy_CacheWarmer_Warmer();
             $warmer->setResolver($resolver);
+
+            //die(print_r($varnishServer['ip']));
             $warmer->setHostname($website->getHostname());
             $warmer->setActor($actor);
 
--- src/Model/VarnishManager.php
+++ src/Model/VarnishManager.php
@@ -29,6 +29,10 @@ class VarnishManager
         $this->varnishWebsiteLinkManager = $varnishWebsiteLinkManager;
     }
 
+    /**
+     * @param User $user
+     * @return array
+     */
     public function getAllByUser(User $user)
     {
         $userId = $user->getUserId();
@@ -39,6 +43,13 @@ class VarnishManager
         return $query->fetchAll(\PDO::FETCH_CLASS, Varnish::class);
     }
 
+    /**
+     * @author Hamza al Darawsheh 24 Sep 2018 <ihamzehald@gmail.com>
+     * @param Varnish $varnish
+     * @return array
+     * Get all websites that linked to a varnish server
+     * Ticket Ref: task 5
+     */
     public function getWebsites(Varnish $varnish)
     {
 
@@ -57,11 +68,41 @@ class VarnishManager
 
     }
 
+    /**
+     * @author Hamza al Darawsheh 25 Sep 2018 <ihamzehald@gmail.com>
+     * @param \Snowdog\DevTest\Model\Website $website
+     * Get the varnish server that linked to a website
+     * Ticket Ref: task 5
+     * Note: The UI allows multiple varnish server to be linked with the same website,
+     * nothing mentioned regarding this in the requiements, in real life scenario i useally
+     * ask the PM on what is the expected behavior, for now i gona get only the first linked varnish,
+     * we can discuss this more at the intereview in case i passed the next step.
+     */
     public function getByWebsite(Website $website)
     {
-        // TODO: add logic here
+        $websiteId = $website->getWebsiteId();
+
+        $query = $this->database->prepare("
+                                          SELECT *
+                                          FROM varnish
+                                          INNER JOIN varnish_website_link
+                                          ON varnish.id = varnish_website_link.varnish_id
+                                          WHERE varnish_website_link.website_id = :website_id
+                                          ");
+        $query->bindParam(':website_id', $websiteId, \PDO::PARAM_INT);
+        $query->execute();
+
+        return $query->fetch();
     }
 
+    /**
+     * @author Hamza al Darawsheh 24 Sep 2018 <ihamzehald@gmail.com>
+     * @param User $user
+     * @param $ip
+     * @return string
+     * Create a varnish server
+     * Ticket Ref: task 5
+     */
     public function create(User $user, $ip)
     {
         $userId = $user->getUserId();
@@ -73,17 +114,39 @@ class VarnishManager
         return $this->database->lastInsertId();
     }
 
-
+    /**
+     * @author Hamza al Darawsheh 24 Sep 2018 <ihamzehald@gmail.com>
+     * @param $varnish
+     * @param $website
+     * @return bool
+     * Link a varnish server to a website
+     * Ticket Ref: task 5
+     */
     public function link($varnish, $website)
     {
         return $this->varnishWebsiteLinkManager->create($varnish->getId(), $website->getWebsiteId());
     }
 
+    /**
+     * @author Hamza al Darawsheh 24 Sep 2018 <ihamzehald@gmail.com>
+     * @param $varnish
+     * @param $website
+     * @return bool
+     * Unlink a website from varnish server
+     * Ticket Ref: task 5
+     */
     public function unlink($varnish, $website)
     {
         return $this->varnishWebsiteLinkManager->delete($varnish->getId(), $website->getWebsiteId());
     }
 
+    /**
+     * @author Hamza al Darawsheh 24 Sep 2018 <ihamzehald@gmail.com>
+     * @param $id
+     * @return mixed
+     * Get one varnish server by varnish_id
+     * Ticket Ref: task 5
+     */
     public function getVarnishById($id){
         $id = intval($id);
         $query = $this->database->prepare("SELECT * FROM varnish WHERE id = {$id}");
